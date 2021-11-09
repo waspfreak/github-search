c2a9fb5abe2f1e8d87421ef71a67ed198501c6a1
juliana <juliana.leon@tigerspike.com>
2021-11-09 10:32:28 +0000

    add item component

---
+++ src/components/Item/Item.tsx
@@ -0,0 +1,60 @@
+import React, { SyntheticEvent } from 'react'
+import { List, ImageStyle } from './style';
+
+export interface ItemProps {
+  label?: string;
+  alt?: string;
+  src?: string;
+  id?: number;
+  starts?: string;
+  user?: string;
+  description?: string;
+  name?: string;
+  url?: string;
+  language?: string;
+  children?: any
+  onClick?: (event: SyntheticEvent) => void;
+}
+
+
+export const Item: React.FC<ItemProps> = ({
+  label,
+  alt,
+  src,
+  id,
+  starts,
+  user,
+  description,
+  name,
+  url,
+  language,
+  onClick,
+  children,
+  ...props
+}) => {
+
+  return (
+
+    <List data-testid="item-id" key={id}>
+      <div>
+        <ImageStyle alt={alt} width="100" src={src} />
+        <span>stars: {starts} </span>
+      </div>
+      <div>
+        <p><span>User: </span>{user}</p>
+        <p><span>Repository:</span> <a href={url}>{name}</a></p>
+        <p><span>Description:</span> {description}</p>
+        <p><span>Language:</span> {language}</p>
+      </div>
+      <div className="favorites" title="save to favs">
+        {children}
+      </div>
+
+
+    </List >
+
+  );
+};
+
+
+export default Item
---
+++ src/components/Item/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const List = styled.li`
+  display: flex;
+  border-bottom: 1px solid blue;
+  margin-bottom: 1em;
+`;
+
+export const ImageStyle = styled.img`
+  display: inline-block;
+  border-radius: 50%;
+`;
